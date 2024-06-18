e9a4bfb2080764492f2cda8e62018f862a76f746
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2024-06-19 02:36:10 +0530

    setup route and controller

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 import express from "express";
+import userRoute from "./src/user/userRoute.js";
 
 const app = express();
+
+// Routes
+app.use("/api/v1/user", userRoute);
 
 export default app;
---
+++ src/user/userController.js
@@ -0,0 +1,4 @@
+// http://localhost:8000/api/v1/user/me
+export const getMyProfile = (req, res, next) => {
+  res.send("Me");
+};
