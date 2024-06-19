c0ad9d81119ff406d751a46c188bbd906e9506e6
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2024-06-19 23:28:32 +0530

    add cookie options

--- src/utils/features.js
+++ src/utils/features.js
@@ -1,9 +1,18 @@
+import { config } from "../config/config.js";
+
+export const cookieOptions = {
+  secure: config.env === "development" ? false : true,
+  httpOnly: config.env === "development" ? false : true,
+  sameSite: config.env === "development" ? false : "none",
+};
+
 export const sendToken = (user, res, message, statusCode) => {
   const token = user.generateToken();
 
   res
     .status(statusCode)
     .cookie("token", token, {
+      ...cookieOptions,
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     })
     .json({
